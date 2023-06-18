fcc5094be39370d17c6a20752e3bd68ee9fd6336
Antony74 <akb@akb.me.uk>
2023-06-18 15:40:36 +0100

    This is probably the wrong approach

--- src/mmParser/tokensToJson.ts
+++ src/mmParser/tokensToJson.ts
@@ -4,6 +4,7 @@ import { TokenStream } from './tokenStream';
 
 export const createTokensToJson = (writer: JsonWriter): TokenStream => {
     let ws: string[] = [];
+    let startProofBlock = false;
 
     writer
         .beginObject()
@@ -12,6 +13,28 @@ export const createTokensToJson = (writer: JsonWriter): TokenStream => {
         .name('children')
         .beginArray();
 
+    const beginDoubleBlock = (
+        outerName: string,
+        tokenName: string,
+        innerName: string
+    ): void => {
+        writer
+            .beginObject()
+            .name('ws')
+            .value(ws)
+            .name('type')
+            .value(outerName)
+            .name('children')
+            .beginArray()
+            .value({ type: tokenName, text: tokenName })
+            .beginObject()
+            .name('type')
+            .value(innerName)
+            .name('children')
+            .beginArray();
+        ws = [];
+    };
+
     const hook = {
         onToken: (token: Token): void => {
             switch (token.type) {
@@ -19,33 +42,56 @@ export const createTokensToJson = (writer: JsonWriter): TokenStream => {
                 case '_COMMENT':
                     ws.push(token.text);
                     break;
-                case '$c':
-                    writer
-                        .beginObject()
-                        .name('ws')
-                        .value(ws)
-                        .name('type')
-                        .value('constant_stmt')
-                        .name('children')
-                        .beginArray()
-                        .value({ type: '$c', text: '$c' })
-                        .beginObject()
-                        .name('type')
-                        .value('constants')
-                        .name('children')
-                        .beginArray();
-                    ws = [];
-                    break;
                 case 'MATH_SYMBOL':
-                    writer.value({ ws, type: token.type, text: token.text });
+                case 'LABEL':
+                    if (startProofBlock) {
+                        writer
+                            .beginObject()
+                            .name('type')
+                            .value('uncompressed_proof')
+                            .name('children')
+                            .beginArray();
+                        startProofBlock = false;
+                    }
+                    writer.value({
+                        ws,
+                        type: token.type,
+                        text: token.text,
+                    });
                     ws = [];
                     break;
                 case '$.':
-                    writer.close();
-                    writer.close();
-                    writer.value({ ws, type: '$.', text: '$.' });
+                case '$}':
+                    writer.close().close();
+                    writer.value({ ws, type: token.type, text: token.text });
                     ws = [];
-                    writer.close();
+                    writer.close().close();
+                    break;
+                case '$=':
+                    writer.close().close();
+                    writer.value({ ws, type: '$p', text: '$p' });
+                    startProofBlock = true;
+                    break;
+                case '$c':
+                    beginDoubleBlock('constant_stmt', '$c', 'constants');
+                    break;
+                case '$v':
+                    beginDoubleBlock('variable_stmt', '$v', 'variables');
+                    break;
+                case '$f':
+                    beginDoubleBlock('floating_stmt', '$f', 'statement');
+                    break;
+                case '$a':
+                    beginDoubleBlock('axiom_stmt', '$a', 'assertion');
+                    break;
+                case '${':
+                    beginDoubleBlock('block', '${', 'statements');
+                    break;
+                case '$e':
+                    beginDoubleBlock('essential_stmt', '$e', 'statement');
+                    break;
+                case '$p':
+                    beginDoubleBlock('provable_stmt', '$p', 'assertion');
                     break;
                 case 'eof':
                     writer.close().name('trailingWs').value(ws).close();
