fd86514f786dd716feeb076ba9a36f0c5889328b
Antony74 <akb@akb.me.uk>
2023-07-15 19:29:39 +0100

    Remove trivially inferable types

--- src/utils/objectMap.ts
+++ src/utils/objectMap.ts
@@ -2,11 +2,8 @@ export const objectMap = <T, U>(
     record: Record<string, T>,
     mapFn: (value: T, key: string) => U
 ): Record<string, U> => {
-    return Object.keys(record).reduce<Record<string, U>>(
-        (acc, key): Record<string, U> => {
-            const value = record[key];
-            return { ...acc, [key]: mapFn(value, key) };
-        },
-        {}
-    );
+    return Object.keys(record).reduce((acc, key) => {
+        const value = record[key];
+        return { ...acc, [key]: mapFn(value, key) };
+    }, {});
 };
