3c68eda720d57245341aae930dc8f550a24db15d
Antony74 <akb@akb.me.uk>
2023-11-09 14:34:07 +0000

    Move ebnf.ebnf

--- src/ebnf.ts
+++ src/ebnf.ts
@@ -5,7 +5,7 @@ import path from 'path';
 import { ebnfLexer } from './ebnf-to-json/ebnf-bootstrap-lexer';
 
 const main = async (): Promise<void> => {
-    const filename = path.join(__dirname, 'ebnf-to-json', 'ebnf.ebnf');
+    const filename = path.join(__dirname, 'ebnf', 'ebnf.ebnf');
     const text = await fsp.readFile(filename, { encoding: 'utf-8' });
     ebnfLexer.reset(text);
     let token;
